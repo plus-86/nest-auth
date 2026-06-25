96de60b2b125fb7bcda3782da0e49d3c21824bc7
Lin <linlgd@outlook.com>
2026-06-25 18:52:39 +0800

    docs: permission注释

--- src/common/decorators/permission.decorator.ts
+++ src/common/decorators/permission.decorator.ts
@@ -1,3 +1,4 @@
+// 这个自定义装饰器 RequirePermissions 的核心作用，就是为后续的 Guard（守卫） 通过 Reflector 读取元数据做准备。
 import { SetMetadata } from '@nestjs/common';
 export const RequirePermissions = (...codes: string[]) =>
     SetMetadata('permissions', codes);
--- src/common/guards/permission.guard.ts
+++ src/common/guards/permission.guard.ts
@@ -12,14 +12,14 @@ export class PermissionsGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     // 1. 从接口上的装饰器取到需要的权限码
     const requiredCodes = this.reflector.get<string[]>(
-      'permissions',
-      context.getHandler(),
+      'permissions', // 找 key 为 'permissions' 的元数据
+      context.getHandler(), // 锁定具体处理方法，然后从那个方法上取出对应 'permissions' 标记的值
     );
     if (!requiredCodes) return true; // 没加装饰器则直接放行
 
     // 2. 从请求里拿到当前登录用户（假设前面有 JWT 守卫已经挂载了 user）
     const { user } = context.switchToHttp().getRequest();
-    // 3. 取用户的所有权限码（需要在JWT策略里提前查好放到 user.permissions 里）
+    // 3. 取用户的所有权限码（需要在JWT策略里提前查好放到 user.xx 里）
     const userPermissions = user.permissionCodes ?? [];
 
     // 4. 检查用户是否拥有任意一个所需权限
